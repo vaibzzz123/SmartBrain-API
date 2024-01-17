82b3e5a33356c4ffa0b817db14b413c770cc7254
Vab Kapoor <vaib.kapoor15@gmail.com>
2024-01-16 19:25:35 -0500

    Added env example file, and cleaned up some unnecessary comments on DB connection line

--- server.js
+++ server.js
@@ -14,15 +14,6 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: process.env.DATABASE_URL
-    // {
-    //     host: process.env.DB_HOST,
-    //     user: process.env.DB_USER,
-    //     password: process.env.DB_PASS,
-    //     database: process.env.DB_DBNAME,
-    //     ssl: {
-    //         rejectUnauthorized: false
-    //     }
-    // }
 });
 
 const port = process.env.PORT || 3001;
